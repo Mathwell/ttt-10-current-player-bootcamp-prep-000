b8cf1178be57102b1f62a60a8801623a656e59df
Olena Ageyeva <eageeva@hotmail.com>
2017-09-26 21:59:48 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,8 +2,7 @@ def turn_count(board)
   count=0
   board.each {|cell|
     if (cell=="X" || cell=="O")
-      count+=1
-      puts "#{cell}"+"#{count}"
+      count+=1      
     end
     }
     return count
